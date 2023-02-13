dec6202916a29bded3598c8d692e34430f54146a
Julian Ward <wardjulianm@gmail.com>
2023-02-13 09:57:19 -0700

    Already finished in prework

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function countDown(num){
+    let countvar = num
+    while(countvar > -.1){
+        console.log(countvar)
+        countvar--
+        debugger
+    }
+}
+
+//countDown(10)
+
+
+function writeCards(namearray, event){
+    var cardarray = []
+    for (let i = 0; i < namearray.length; i++){
+        var cardcont = `Thank you, ${namearray[i]}, for the wonderful ${event} gift!`;
+        cardarray.push(cardcont);}
+    return cardarray}
+
+//console.log(writeCards(['Test', 'Name', 'Testy Name'], 'Christmas'))
